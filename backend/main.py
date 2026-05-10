7ba6563ae8ca81b38f086db0d30fde4cdf6d1505
Joel Ha. <joelharder4@gmail.com>
2026-05-09 20:49:40 -0400

    database connection stuff

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,31 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from sqlmodel import Field, Session, SQLModel, create_engine
+from dotenv import load_dotenv
+import os
 
-app = FastAPI()
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+engine = create_engine(DATABASE_URL)
+
+class ChatEntry(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    user_id: int
+    prompt: str
+    response: str
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+    # Any teardown code here
+
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def default_endpoint():
